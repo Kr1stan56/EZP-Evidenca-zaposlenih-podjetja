358e04e1924d667d514acde3db6c3bf2e24fbb9f
Kr1stan <117606644+Kr1stan56@users.noreply.github.com>
2026-01-27 09:39:19 +0100

    minor changes

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Moja aplikacija");
+        frame.setSize(400, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
