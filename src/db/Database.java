e9c0cb66a8257c9563dbaf039bd11509c808e029
Kristan <117606644+Kr1stan56@users.noreply.github.com>
2026-02-02 22:04:40 +0100

    baza supbase

--- src/db/Database.java
+++ src/db/Database.java
@@ -7,9 +7,13 @@ import java.sql.SQLException;
 public class Database {
     private Connection conn;
 
-    private final String url  = "jdbc:postgresql://wiggly-bug-21257.j77.aws-eu-central-1.cockroachlabs.cloud:26257/ezp?sslmode=verify-full&password=<ENTER-SQL-USER-PASSWORD>A5JsvVSQWZQWsHWxSQEBIg&user=postgres";
-    private final String user = "postgres";
-    private final String pass = "A5JsvVSQWZQWsHWxSQEBIg";
+    private final String url =
+            "jdbc:postgresql://aws-1-eu-west-1.pooler.supabase.com:5432/postgres?sslmode=require";
+
+    private final String user = "postgres.vuhabmtcbwlafsfwzyzu";
+    private final String pass = "V6GFzDJ4Np2K2YkO";
+
+
 
     public void connect() throws SQLException, ClassNotFoundException {
         Class.forName("org.postgresql.Driver");
@@ -20,7 +24,6 @@ public class Database {
 
     public Connection getConnection() throws SQLException, ClassNotFoundException {
         if (conn == null || conn.isClosed()) {
-            // če je zaprta ali null, jo avtomatsko ponovno odpri
             try {
                 connect();
             } catch (SQLException | ClassNotFoundException e) {
