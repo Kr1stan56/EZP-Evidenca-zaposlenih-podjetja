8c460731bea57f3b497257741619f2c3cddb25f2
Kristan <117606644+Kr1stan56@users.noreply.github.com>
2026-02-02 20:24:09 +0100

    testing host

--- src/db/Database.java
+++ src/db/Database.java
@@ -7,9 +7,9 @@ import java.sql.SQLException;
 public class Database {
     private Connection conn;
 
-    private final String url  = "jdbc:postgresql://localhost:5432/postgres";
+    private final String url  = "jdbc:postgresql://wiggly-bug-21257.j77.aws-eu-central-1.cockroachlabs.cloud:26257/ezp?sslmode=verify-full&password=<ENTER-SQL-USER-PASSWORD>A5JsvVSQWZQWsHWxSQEBIg&user=postgres";
     private final String user = "postgres";
-    private final String pass = "root";
+    private final String pass = "A5JsvVSQWZQWsHWxSQEBIg";
 
     public void connect() throws SQLException, ClassNotFoundException {
         Class.forName("org.postgresql.Driver");
